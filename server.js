3efe63f0540977facbf4ea0b76ebcebe0e0772f4
nyanko4 <nyankobetsuaka@outlook.com>
2025-08-13 12:10:15 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // ルート登録
-app.get("/", (req, res) => {
-  res.send(`
-    <a href='/member/proxy'>proxy</a><br>
-    <a href='/member/renrenproxy'>れんにゃんproxy</a><br>
-    <a href='/member/senden'>宣伝部屋</a><br>
-  `);
-})
-app.use("/member", require("./routes/member"))
 app.use("/", require("./routes/webhook"));
 app.get('/send', (req, res) => {
   res.end(JSON.stringify(process.versions, null, 2));

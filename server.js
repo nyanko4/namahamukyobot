3d7ae51f99616b707859f3bfe72f93c0c5912cf7
nyanko4 <nyankobetsuaka@outlook.com>
2025-10-19 16:06:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ const app = express();
 // ミドルウェア設定
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // ルート登録
 app.use("/", require("./routes/webhook"));
 app.get('/send', (req, res) => {
   res.end(JSON.stringify(process.versions, null, 2));
   console.log("ぬ")
 });
-
 // サーバ起動
 app.listen(3000, () => {
   console.log(`${process.pid} started`);

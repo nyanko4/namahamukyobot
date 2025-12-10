9da41494691fb4090cf71c1ae67a880084d1f39f
nyanko4 <nyankobetsuaka@outlook.com>
2025-12-10 14:07:19 +0900

    Update omikuji.js

--- module/omikuji.js
+++ module/omikuji.js
@@ -36,7 +36,7 @@ async function getOmikujiResult(accountId) {
 
 //おみくじ
 async function omikuji(body, messageId, roomId, accountId) {
-  if (body.match(/^おみくじ$|^布瑠部由良由良八握剣異戒神将御神籤$/)) {
+  if (/^おみくじ$/.test(body) || (/^布瑠部由良由良八握剣異戒神将御神籤$/.test(body) && accountId === 10787667)) {
     try {
       const { data, error } = await supabase
         .from("おみくじ")
